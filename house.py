bb63fd1d8dbaea7a006008d4a809dc6eff987ba4
Enis Ozgit <eozgit@gmail.com>
2020-12-14 20:52:01 +0000

    Implement token authentication, convert exposed object keys to camel case

--- house.py
+++ house.py
@@ -1,49 +1,69 @@
 from flask import Flask
 from flask_restx import Api, Resource
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from box import Box
 from model.orm import db
 from model.company import Company
 from serialize.company import company
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 
 
 db.init_app(app)
 api = Api(app)
+jwt = JWTManager(app)
 
 
 api.models[company.name] = company
 
 
-@api.route('/house')
-class House(Resource):
-    def get(self):
-        return {'house': 'companies'}
+@api.route('/login')
+class Login(Resource):
+    def post(self):
+        payload = Box(api.payload)
+        username = payload.username
+        password = payload.password
+
+        if not username:
+            return {'message': 'Missing username parameter'}, 400
+        if not password:
+            return {'message': 'Missing password parameter'}, 400
+
+        if username != 'test' or password != 'test':
+            return {'message': 'Bad username or password'}, 401
+
+        access_token = create_access_token(identity=username)
+        return {'access_token': access_token}, 200
 
 
 @api.route('/company/<string:id>')
 class CompanyApi(Resource):
     @api.marshal_with(company)
+    @jwt_required
     def get(self, id):
         return Company.query.get_or_404(id)
 
     @api.marshal_with(company)
+    @jwt_required
     def put(self, id):
         payload = Box(api.payload)
-        company = Company.query.get(id)
-        company.company_name = payload.company_name
-        company.reg_address_address_line1 = payload.reg_address_address_line1
-        company.reg_address_address_line2 = payload.reg_address_address_line2
-        company.reg_address_post_town = payload.reg_address_post_town
-        company.reg_address_county = payload.reg_address_county
-        company.reg_address_country = payload.reg_address_country
-        company.reg_address_post_code = payload.reg_address_post_code
-        company.company_category = payload.company_category
-        company.company_status = payload.company_status
-        company.country_of_origin = payload.country_of_origin
-        company.sic_code_sic_text_1 = payload.sic_code_sic_text_1
+        company = Company.query.get_or_404(id)
+
+        company.company_name = payload.companyName
+        company.reg_address_address_line1 = payload.addressLine1
+        company.reg_address_address_line2 = payload.addressLine2
+        company.reg_address_post_town = payload.addressPostTown
+        company.reg_address_county = payload.addressCounty
+        company.reg_address_country = payload.addressCountry
+        company.reg_address_post_code = payload.addressPostCode
+        company.company_category = payload.companyCategory
+        company.company_status = payload.companyStatus
+        company.country_of_origin = payload.countryOfOrigin
+        company.sic_code_sic_text_1 = payload.sicCode
         company.uri = payload.uri
         db.session.commit()
         return company
