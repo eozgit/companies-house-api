7098b0b87c98069a98dd868fc3bd77dbda081f44
Enis Ozgit <eozgit@gmail.com>
2020-12-28 15:42:22 +0000

    Test psc update, read & delete methods

--- tests/test_psc.py
+++ tests/test_psc.py
@@ -46,3 +46,60 @@ def test_psc_create(auth):
     person = Box(person)
     assert person.name == 'Jane Doe'
     assert person.yearOfBirth == 1980
+
+
+def test_psc_update(auth):
+    with app.app_context():
+        record = Person.query.filter(
+            Person.name == 'Jane Doe').first()
+
+    client, headers = auth
+    response = client.put(
+        f'/company/10919507/person/{record.id}', headers=headers, json={
+            'yearOfBirth': 1985,
+            'name': 'Jane Doe',
+            'nationality': 'Kenyan',
+            'naturesOfControl': [
+                {
+                    'natureOfControl': 'ownership-of-shares-75-to-100-percent'
+                },
+                {
+                    'natureOfControl': 'voting-rights-75-to-100-percent'
+                }
+            ],
+            'notifiedOn': '2020-12-28'
+        })
+    person = response.get_json()
+    person = Box(person)
+    assert person.yearOfBirth == 1985
+    assert len(person.naturesOfControl) == 2
+
+
+def test_psc_read(auth):
+    with app.app_context():
+        record = Person.query.filter(
+            Person.name == 'Jane Doe').first()
+
+    client, headers = auth
+    response = client.get(
+        f'/company/10919507/person/{record.id}', headers=headers)
+    person = response.get_json()
+    person = Box(person)
+    assert person.yearOfBirth == record.dob_year
+    assert person.nationality == record.nationality
+
+
+def test_psc_delete(auth):
+    with app.app_context():
+        record = Person.query.filter(
+            Person.name == 'Jane Doe').first()
+
+    client, headers = auth
+    response = client.delete(
+        f'/company/10919507/person/{record.id}', headers=headers)
+
+    with app.app_context():
+        record = Person.query.filter(
+            Person.id == record.id).first()
+
+    assert record is None
