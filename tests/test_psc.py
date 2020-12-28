0168c558b20da7026de63142bb55c018b70b9de1
Enis Ozgit <eozgit@gmail.com>
2020-12-28 12:18:24 +0000

    Test psc create method

--- tests/test_psc.py
+++ tests/test_psc.py
@@ -1,6 +1,8 @@
 from box import Box
 from fixture_authorization import auth
+from app import app
 from model.orm import db
+from model.person import Person
 
 
 def test_psc_search(auth):
@@ -11,3 +13,36 @@ def test_psc_search(auth):
     assert isinstance(people, list)
     person = Box(people[0])
     assert person.name == 'Mr. Alejandro Ruiz Rodriguez'
+
+
+def test_psc_create(auth):
+    with app.app_context():
+        test_data = Person.query.filter(
+            Person.name == 'Jane Doe').all()
+        for p in test_data:
+            db.session.delete(p)
+        db.session.commit()
+
+    client, headers = auth
+    response = client.post(
+        '/company/10919507/person', headers=headers, json={
+            'yearOfBirth': 1980,
+            'name': 'Jane Doe',
+            'nationality': 'Kenyan',
+            'naturesOfControl': [
+                {
+                    'natureOfControl': 'ownership-of-shares-75-to-100-percent'
+                },
+                {
+                    'natureOfControl': 'voting-rights-75-to-100-percent'
+                },
+                {
+                    'natureOfControl': 'right-to-appoint-and-remove-directors'
+                }
+            ],
+            'notifiedOn': '2020-12-28'
+        })
+    person = response.get_json()
+    person = Box(person)
+    assert person.name == 'Jane Doe'
+    assert person.yearOfBirth == 1980
